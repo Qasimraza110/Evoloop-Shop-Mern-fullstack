0ed9a78c5f96e5f3b7d59d0061901d744db6924c
Qasim Raza <sh.qasim.raza110@gmail.com>
2025-09-09 16:11:25 +0500

    Update Product.js

--- Evoloop Shop/backend/models/Product.js
+++ Evoloop Shop/backend/models/Product.js
@@ -4,7 +4,8 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String, default: "" },
-  image: { type: String, default: "" }
+  image: { type: String, default: "" },
+  stock: { type: Number, required: true, default: 10 } 
 }, { timestamps: true });
 
 export default mongoose.model("Product", productSchema);
