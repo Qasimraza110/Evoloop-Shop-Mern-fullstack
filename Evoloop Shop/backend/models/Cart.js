857767635b47e58fdb77200d092a7e6c2a8a50aa
Qasim Raza <sh.qasim.raza110@gmail.com>
2025-09-11 13:26:56 +0500

    Delete Evoloop Shop directory

--- Evoloop Shop/backend/models/Cart.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const cartSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  items: [{ _id: String, name: String, price: Number, quantity: Number }]
-});
-
-export default mongoose.model("Cart", cartSchema);
