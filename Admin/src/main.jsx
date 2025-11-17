77439f998384450118f6e0a704eb06c03c277675
Qasim Raza <sh.qasim.raza110@gmail.com>
2025-11-17 02:31:30 -0800

    Fix indentation in AuthProvider component

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -6,7 +6,7 @@ import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <AuthProvider>
+   <AuthProvider> 
   <App />
 </AuthProvider>
   </StrictMode>,
